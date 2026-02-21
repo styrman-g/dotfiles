45833ca6a7a1ee044a4295e539879e57eb411f60
styrman <styrman@bettan.home>
2026-02-21 15:03:46 +0100

    fix neovim

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -58,17 +58,6 @@ require("lazy").setup({
     -- refer to the configuration section below
   }
 },
-  {
-    "vimwiki/vimwiki",
-    init = function()
-      vim.g.vimwiki_list = {
-        { path = '~/Dokument/vimwiki', syntax = 'markdown', ext = '.md', links_space_char = '-' },
-      }
-      vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
-      vim.g.vimwiki_use_mouse = 1
-      vim.g.vimwiki_markdown_link_ext = 1
-    end,
-  },
  {
    "s1n7ax/nvim-terminal",
     init = function()
@@ -84,19 +73,8 @@ require("lazy").setup({
   -- dependencies = { "echasnovski/mini.icons" },
   opts = {}
 },
-  opts = {
-    workspaces = {
-      {
-        name = "personal",
-        path = "~/Dokument/second-brain/",
-      },
-    },
+})
 
-    -- see below for full list of options 👇
-  },
-},
-
-      ) ),
 
 
 
