360506aeac88a2d20c8b55be492dd094844d2c38
Gustav Hogbring <gustav.hogbring@proton.me>
2026-06-13 13:03:38 +0200

    update picom

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+picom &
 nm-applet &
 emacs --daemon &
 blueman-applet &
-flatpak run com.protonvpn.www &
 flatpak run com.synology.SynologyDrive start &
-flatpak run org.signal.Signal --enable-features=UseOzonePlatform --ozone-platform=wayland &
+flatpak run org.signal.Signal &
 volumeicon &
